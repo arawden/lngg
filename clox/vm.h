c13ee51cd359b38541d96d0a4f3eb9a42e5cebb9
avery <avery@gitblame.me>
2023-07-30 19:16:37 -0700

    `chapter 21`: globals (#6)
    * section 1 - statements
    
    * var declarations
    
    * var declarations
    
    * basic read/write

--- clox/vm.h
+++ clox/vm.h
@@ -7,21 +7,20 @@
 
 #define STACK_MAX 256
 
-typedef struct
-{
-  Chunk *chunk;
-  uint8_t *ip; // instruction pointer
-  Value stack[STACK_MAX];
-  Value *stackTop; // points where next value will be pushed to
-  Table strings;   // interned strings
-  Obj *objects;    // pointer to head of list of allocated objects
+typedef struct {
+    Chunk *chunk;
+    uint8_t *ip;  // instruction pointer
+    Value stack[STACK_MAX];
+    Value *stackTop;  // points where next value will be pushed to
+    Table globals;
+    Table strings;  // interned strings
+    Obj *objects;   // pointer to head of list of allocated objects
 } VM;
 
-typedef enum
-{
-  INTERPRET_OK,
-  INTERPRET_COMPILE_ERROR,
-  INTERPRET_RUNTIME_ERROR
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
 } InterpretResult;
 
 extern VM vm;
