88223384849fe58d2332c1a2c2db6e054b460e32
avery <avery@gitblame.me>
2023-09-05 00:20:44 +0000

    sketch functions, setting tweaks

--- clox/object.c
+++ clox/object.c
@@ -1,16 +1,16 @@
+#include "object.h"
+
 #include <stdio.h>
 #include <string.h>
 
 #include "memory.h"
-#include "object.h"
 #include "value.h"
 #include "vm.h"
 
 #define ALLOCATE_OBJ(type, objectType) \
     (type *)allocateObject(sizeof(type), objectType)
 
-static Obj *allocateObject(size_t size, ObjType type)
-{
+static Obj *allocateObject(size_t size, ObjType type) {
     Obj *object = (Obj *)reallocate(NULL, 0, size);
     object->type = type;
 
@@ -20,8 +20,16 @@ static Obj *allocateObject(size_t size, ObjType type)
     return object;
 }
 
-static ObjString *allocateString(char *chars, int length, uint32_t hash)
-{
+ObjFunction *newFunction() {
+    ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+    function->arity = 0;
+    function->name = NULL;
+    initChunk(&function->chunk);
+
+    return function;
+}
+
+static ObjString *allocateString(char *chars, int length, uint32_t hash) {
     ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
@@ -32,23 +40,19 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash)
     return string;
 }
 
-static uint32_t hashString(const char *key, int length)
-{
+static uint32_t hashString(const char *key, int length) {
     uint32_t hash = 2166136261u;
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         hash ^= (uint8_t)key[i];
         hash *= 16777619;
     }
     return hash;
 }
 
-ObjString *takeString(char *chars, int length)
-{
+ObjString *takeString(char *chars, int length) {
     uint32_t hash = hashString(chars, length);
     ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
-    if (interned != NULL)
-    {
+    if (interned != NULL) {
         FREE_ARRAY(char, chars, length + 1);
 
         return interned;
@@ -57,13 +61,11 @@ ObjString *takeString(char *chars, int length)
     return allocateString(chars, length, hash);
 }
 
-ObjString *copyString(const char *chars, int length)
-{
+ObjString *copyString(const char *chars, int length) {
     uint32_t hash = hashString(chars, length);
 
     ObjString *interned = tableFindString(&vm.strings, chars, length, hash);
-    if (interned != NULL)
-        return interned;
+    if (interned != NULL) return interned;
 
     char *heapChars = ALLOCATE(char, length + 1);
     memcpy(heapChars, chars, length);
@@ -73,12 +75,17 @@ ObjString *copyString(const char *chars, int length)
     return allocateString(heapChars, length, hash);
 }
 
-void printObject(Value value)
-{
-    switch (OBJ_TYPE(value))
-    {
-    case OBJ_STRING:
-        printf("%s", AS_CSTRING(value));
-        break;
+static void printFunction(ObjFunction *function) {
+    printf("<fn %s>", function->name->chars);
+}
+
+void printObject(Value value) {
+    switch (OBJ_TYPE(value)) {
+        case OBJ_FUNCTION:
+            printFunction(AS_FUNCTION(value));
+            break;
+        case OBJ_STRING:
+            printf("%s", AS_CSTRING(value));
+            break;
     }
 }
